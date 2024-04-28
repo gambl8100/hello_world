cf100d8ad55e2e107b420b33f5514607a026aa49
Valera <151994525+gambl8100@users.noreply.github.com>
2024-04-29 01:50:55 +0400

    more examples

--- marks.py
+++ marks.py
@@ -2,3 +2,14 @@ marks = [1, 2, 3, 4, 5]
 
 total_marks = sum(marks)
 print(total_marks)
+
+marks1 = [1, 2, 3, 4, 5, 6]
+
+total_marks1 = sum(marks1)
+print(total_marks1)
+
+
+marks2 = [1, 2, 3, 4, 5, 6, 7]
+
+total_marks2 = sum(marks2)
+print(total_marks2)
